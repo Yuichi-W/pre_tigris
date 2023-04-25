b3be07db55b31130327656b1451bf57e9e64f12d
y.wada <wada1257@gmail.com>
2023-04-25 22:39:09 +0900

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin={"crossorigin"}
+          crossOrigin={undefined}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600&display=swap"
